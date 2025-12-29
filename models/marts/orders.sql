99e07135b2e70d8f22a26e2c5efbe75fc15c65b2
Ryan Quigley <ryan.quigley@atlan.com>
2025-12-29 17:34:06 +0000

    Old references to ordered_at

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -67,7 +67,7 @@ customer_order_count as (
 
         row_number() over (
             partition by customer_id
-            order by ordered_at asc
+            order by ordered_at_pt asc
         ) as customer_order_number
 
     from compute_booleans
